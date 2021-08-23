d5ca77735b4f84da0ad3b5f6980e9bae53e9b68e
M4tasG <grybauskas.matas@gmail.com>
2021-08-23 12:37:33 +0300

    removed whitespace from values

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 url = 'https://koronastop.lrv.lt/'
 page = BeautifulSoup(requests.get(url).content, 'html.parser')
 stats = page.find('div', class_='stats_list').find_all('div', class_='stats_item')
@@ -8,3 +9,4 @@ for stat in stats:
     title = stat.find('div', class_='title')
     value = stat.find('div', class_='value')
     print(f'{title.text}: {value.text.strip()}')
+
