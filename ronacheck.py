094c19cd7c3f9d4b5c9853ae3a986e72acb958ea
M4tasG <grybauskas.matas@gmail.com>
2021-08-23 13:06:59 +0300

    added more functionality to ronacheck

--- ronacheck.py
+++ ronacheck.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+url = 'https://koronastop.lrv.lt/'
+page = BeautifulSoup(requests.get(url).content, 'html.parser')
+stats = page.find('div', class_='stats_list').find_all('div', class_='stats_item')
 
 def get_ronastats():
-    url = 'https://koronastop.lrv.lt/'
-    page = BeautifulSoup(requests.get(url).content, 'html.parser')
-    stats = page.find('div', class_='stats_list').find_all('div', class_='stats_item')
     stats_arr = []
     for stat in stats:
         title = stat.find('div', class_='title')
@@ -12,3 +12,16 @@ def get_ronastats():
         #stats_arr += f'{title.text}: {value.text.strip()}'
         stats_arr.append(f'{title.text}: {value.text.strip()}')
     return stats_arr
+
+def get_ronacases():
+    return stats[0].find('div', class_="value").text.strip()
+
+def get_ronadeaths():
+    return stats[1].find('div', class_="value").text.strip()
+
+def get_ronapositive():
+    return stats[3].find('div', class_="value").text.strip()
+
+def get_ronavaccinated():
+    return stats[4].find('div', class_="value").text.strip()
+
