db783de6baacda61bef9285ee23fec88ef55bf7d
M4tasG <grybauskas.matas@gmail.com>
2021-08-23 13:01:46 +0300

    cleaned up main.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from bs4 import BeautifulSoup
-import requests
 from ronacheck import get_ronastats
 from win10toast import ToastNotifier
-"""
-url = 'https://koronastop.lrv.lt/'
-page = BeautifulSoup(requests.get(url).content, 'html.parser')
-stats = page.find('div', class_='stats_list').find_all('div', class_='stats_item')
-for stat in stats:
-    title = stat.find('div', class_='title')
-    value = stat.find('div', class_='value')
-    print(f'{title.text}: {value.text.strip()}')
-"""
+
 stats = get_ronastats()
 
 complete_string = f"""
