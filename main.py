2cc52d0df7c278e02454b120c6fb99b472ec3242
M4tasG <grybauskas.matas@gmail.com>
2021-08-23 12:20:49 +0300

    data scraping done, prints out results

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import requests
 
 url = 'https://koronastop.lrv.lt/'
 page = BeautifulSoup(requests.get(url).content, 'html.parser')
-print(page)
+stats = page.find('div', class_='stats_list').find_all('div', class_='stats_item')
+for stat in stats:
+    title = stat.find('div', class_='title')
+    value = stat.find('div', class_='value')
+    print(f'{title.text}: {value.text.strip()}')
