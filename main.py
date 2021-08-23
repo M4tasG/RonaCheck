e4b27beb033bab478cd62dbce5370b27b4a74043
M4tasG <grybauskas.matas@gmail.com>
2021-08-23 12:48:46 +0300

    added a win10 notification with most important data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+from win10toast import ToastNotifier
 
 url = 'https://koronastop.lrv.lt/'
 page = BeautifulSoup(requests.get(url).content, 'html.parser')
@@ -10,3 +10,10 @@ for stat in stats:
     value = stat.find('div', class_='value')
     print(f'{title.text}: {value.text.strip()}')
 
+complete_string = f"""
+{stats[0].find('div', class_='title').text}: {stats[0].find('div', class_='value').text.strip()}
+{stats[1].find('div', class_='title').text}: {stats[1].find('div', class_='value').text.strip()}
+"""
+
+toast = ToastNotifier()
+toast.show_toast("RonaCheck",complete_string,duration=10)
